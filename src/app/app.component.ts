175db5496c621706f565a40beca1306b9e8f0af8
Andrea1904 <andrea.lopez1904@gmail.com>
2021-09-04 18:40:50 -0500

    the first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  items =['nicolas','julian',''];
+
+  products = [
+    {
+      id:'1',
+      image:'assets/images/245px-CapAward.png',
+      title:'Camiseta',
+      price: 80000,
+      description: 'bla bla bla'
+    },
+    {
+      id:'2',
+      image:'assets/images/450_1000.jpg',
+      title:'Prueba 1',
+      price: 80000,
+      description: 'bla bla bla'
+    },
+    {
+      id:'3',
+      image:'assets/images/1481.jpg',
+      title:'Prueba 2',
+      price: 80000,
+      description: 'bla bla bla'
+    },
+
+  ]
+
+addItem(){
+    this.items.push('nuevo item');
+}
+
+deleteItem(index:number){
+  this.items.splice(index, 1);
+}
+clickproduct(id:number){
+   console.log('product');
+   console.log(id);
+}
+
 }
